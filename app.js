20e6d3ff37553ba0de12591d3010818bb5e459b0
Rohit Agharkar <rohitsa40@gmail.com>
2020-12-06 18:04:58 +0530

    Access only front-end heroku

--- app.js
+++ app.js
@@ -15,26 +15,15 @@ const uri = process.env.URI;
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({origin:'https://shark-vr.herokuapp.com/'}));
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", '*');
+//     res.header("Access-Control-Allow-Credentials", true);
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+//     next();
+// });
 
 //Listening to port 9000
 app.listen(port, function () {
