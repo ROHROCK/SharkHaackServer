9fa71b0ac8d69dfe5eb54be9f24c3cc234ced739
Rohit Agharkar <rohitsa40@gmail.com>
2020-12-06 15:08:45 +0530

    ready for production

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors({ origin: 'http://localhost:3000' }));
 //Listening to port 9000
-app.listen('9000', function () {
+app.listen(port, function () {
     console.log("Server Started at port 9000"); 
 });
 
