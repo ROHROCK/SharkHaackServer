83bd7269d0c3d53fa6fcb1c1bae48faef7fcbf41
Rohit Agharkar <rohitsa40@gmail.com>
2020-12-06 15:28:48 +0530

    STupid

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-// const cors = require('cors');
+const cors = require('cors');
 const port = process.env.PORT || 3000
 require('dotenv').config();
 
